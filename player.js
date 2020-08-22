34f123f18749ab3c4052ed394cae884507d5fbb1
vaishnaviagarwal27 <61281973+vaishnaviagarwal27@users.noreply.github.com>
2020-08-22 12:40:07 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,44 @@
+class Player{
+    constructor(){
+       this.index = null;
+       this.distance = 0;
+       this.name = null;
+    }
+      getCount(){
+        database.ref('playerCount').on("value",function(data){
+            playerCount=data.val();
+        });
+    
+      }
+     
+    
+    updateCount(count){
+        database.ref('/').update({
+          playerCount:count
+        })
+    }
+    update(){
+        var playerIndex = "players/player"+this.index;
+        database.ref(playerIndex).set({
+          name:this.name,
+          distance: this.distance
+        })
+    }
+    static getPlayerInfo(){
+        database.ref('players').on("value",function(data){
+            allPlayers = data.val();
+        });
+
+
+    }
+     /* start(){
+      if(gameSate===0){
+          player=new Player();
+          player.getCount();
+          form = new Form();
+          form.display();
+      }
+    */
+      }
+    
+    
